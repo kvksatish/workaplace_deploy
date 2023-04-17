e2bd722504661c5474354b5ff5132910d55c1e5f
unknown <kvksatish98@gmail.com>
2023-04-17 16:15:16 +0530

    newww

---
+++ Models/Lead.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose")
+const LeadSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    comapanyname: {
+        type: String,
+        required: true,
+    },
+    workemail: {
+        type: String,
+        required: true,
+    },
+    phonenumber: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        required: true,
+    },
+    others: {
+        type: String,
+        required: true,
+    }
+})
+const LeadModel = mongoose.model("lead", LeadSchema)
+
+
+module.exports = { LeadModel }

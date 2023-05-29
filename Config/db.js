9d9abe34a90ccf7954996690cc13323b98304f05
unknown <kvksatish98@gmail.com>
2023-05-29 22:43:00 +0530

    nnnnweewe

--- Config/db.js
+++ Config/db.js
@@ -1,7 +1,9 @@
-const mongoose = require("mongoose")
-require("dotenv").config()
-mongoose.set('strictQuery', false);
+const mongoose = require("mongoose");
+require("dotenv").config();
+mongoose.set("strictQuery", false);
 
-const connection = mongoose.connect(process.env.MONGO_URL)
+const connection = mongoose.connect(
+  "mongodb+srv://kvksatish:kvksatish@cluster0.ueotde6.mongodb.net/workaplace?retryWrites=true&w=majority"
+);
 
-module.exports = { connection }
+module.exports = { connection };
